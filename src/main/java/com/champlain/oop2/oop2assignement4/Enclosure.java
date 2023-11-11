0158344601e8e29098a1fd9b105afda5228e6282
bujoldzack <zbujold_26@hotmail.ca>
2023-11-11 10:43:14 -0500

    Enclosure Class

---
+++ src/main/java/com/champlain/oop2/oop2assignement4/Enclosure.java
@@ -0,0 +1,30 @@
+package com.champlain.oop2.oop2assignement4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Enclosure implements AnimalCollection {
+
+    private String aName;
+
+    private final List<Animal> aAnimals = new ArrayList<Animal>();
+
+    @Override
+    public String showAnimals() {
+        StringBuilder allAnimals = new StringBuilder();
+        for (Animal animal : aAnimals) {
+            allAnimals.append(animal.toString());
+        }
+        return allAnimals.toString();
+    }
+
+    @Override
+    public void setName(String pName) {
+        aName = pName;
+    }
+
+    @Override
+    public String getName() {
+        return this.aName;
+    }
+}
